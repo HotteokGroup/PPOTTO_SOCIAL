5f64c3b46be4f3694bd79f9788cf2ad51dc85c61
argon1025 <argon1025@gmail.com>
2023-09-18 00:44:30 +0900

    feat: Swagger 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: `environments/.env.${process.env.NODE_ENV}` })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
+import { writeFileSync } from 'fs';
+
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const servicePort = configService.getOrThrow<string>('SERVICE_PORT');
+  const environment = configService.getOrThrow<string>('ENVIRONMENT');
+
+  if (environment !== 'prod') {
+    const config = new DocumentBuilder()
+      .setTitle('PPOTTO Social API')
+      .setDescription('뽀또 소셜서비스')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, { customSiteTitle: 'Social Service API' });
+    // 로컬 개발환경의 경우 OpenAPI JSON을 확인할 수 있도록 저장
+    if (environment === 'local') {
+      writeFileSync('open-api/open-api-spec.json', JSON.stringify(document));
+    }
+  }
+
+  await app.listen(servicePort);
 }
 bootstrap();

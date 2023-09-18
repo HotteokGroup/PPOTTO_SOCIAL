d1ed77a5fbc1552a479c7119c280eceb1c1a02ca
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2023-09-18 02:42:16 +0000

    [#1] 소셜 서비스 구성 (#2)
    * chore: 로컬환경 도커구성
    
    * feat: Swagger 구성
    
    * feat: 커스텀로거 센트리 연동
    
    * fix: 센트리 DSN 고정값 수정
    
    * fix: 로그 컨텍스트 기본값 수정
    
    * feat: 프리즈마 구성
    
    * feat: ExceptionFilter, Serializer 구성
    
    * feat: CQRS 패키지 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
-import { Module } from '@nestjs/common';
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  Module,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE, APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AllExceptionsFilter } from './lib/exception/all-exception.filter';
+import { ERROR_CODE } from './lib/exception/error.constant';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: `environments/.env.${process.env.NODE_ENV}` })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true, // transform payload to DTO
+        forbidUnknownValues: true, // throw error if unknown properties are present
+        exceptionFactory: (errors: ValidationError[]) => {
+          if (!errors[0]?.constraints) return new BadRequestException(ERROR_CODE.INVALID_DATA);
+          const firstKey = Object.keys(errors[0].constraints)[0];
+          throw new BadRequestException({ ...ERROR_CODE.INVALID_DATA, message: errors[0].constraints[firstKey] });
+        },
+      }),
+    },
+    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,45 @@
+import { writeFileSync } from 'fs';
+
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
 
 import { AppModule } from './app.module';
+import { CustomLogger } from './lib/logger/custom-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  const configService = app.get<ConfigService>(ConfigService);
+  const servicePort = configService.getOrThrow<string>('SERVICE_PORT');
+  const environment = configService.getOrThrow<string>('ENVIRONMENT');
+  const sentryDsn = configService.getOrThrow<string>('SENTRY_DSN');
+  app.useLogger(new CustomLogger());
+
+  if (environment !== 'local') {
+    Sentry.init({
+      dsn: sentryDsn,
+      environment,
+    });
+    app.use(Sentry.Handlers.requestHandler());
+  }
+
+  if (environment !== 'prod') {
+    const config = new DocumentBuilder()
+      .setTitle('PPOTTO Social API')
+      .setDescription('뽀또 소셜서비스')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, { customSiteTitle: 'Social Service API' });
+    // 로컬 개발환경의 경우 OpenAPI JSON을 확인할 수 있도록 저장
+    if (environment === 'local') {
+      writeFileSync('open-api/open-api-spec.json', JSON.stringify(document));
+    }
+  }
+
+  await app.listen(servicePort);
 }
 bootstrap();

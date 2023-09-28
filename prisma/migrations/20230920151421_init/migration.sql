d525c894501b0a29eec34436ccb4f15cec1c6509
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2023-09-28 14:46:19 +0000

    [#6] 공유앨범 초대코드 스키마 구성 (#7)
    * feat: 초대코드 스키마 구성
    
    * feat: 초대코드 유니크 처리

--- prisma/migrations/20230920151421_init/migration.sql
+++ prisma/migrations/20230920151421_init/migration.sql
@@ -35,7 +35,7 @@ CREATE TABLE `ShareAlbumMember` (
 -- CreateTable
 CREATE TABLE `ShareAlbumHistory` (
     `id` VARCHAR(191) NOT NULL,
-    `type` ENUM('CREATE_SHARE_ALBUM', 'UPDATE_SHARE_ALBUM', 'DELETE_SHARE_ALBUM', 'JOIN_SHARE_ALBUM', 'LEAVE_SHARE_ALBUM', 'INVITE_SHARE_ALBUM', 'REMOVE_SHARE_ALBUM') NOT NULL,
+    `type` ENUM('CREATE_SHARE_ALBUM', 'UPDATE_SHARE_ALBUM', 'DELETE_SHARE_ALBUM', 'INVITE_SHARE_ALBUM', 'JOIN_SHARE_ALBUM', 'LEAVE_SHARE_ALBUM') NOT NULL,
     `shareAlbumId` VARCHAR(191) NOT NULL,
     `contentId` VARCHAR(191) NULL,
     `userId` INTEGER NULL,
@@ -47,8 +47,26 @@ CREATE TABLE `ShareAlbumHistory` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `ShareAlbumInviteCode` (
+    `id` VARCHAR(191) NOT NULL,
+    `shareAlbumId` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `ShareAlbumInviteCode_code_key`(`code`),
+    INDEX `ShareAlbumInviteCode_id`(`id`),
+    INDEX `ShareAlbumInviteCode_code`(`code`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `ShareAlbumMember` ADD CONSTRAINT `ShareAlbumMember_shareAlbumId_fkey` FOREIGN KEY (`shareAlbumId`) REFERENCES `ShareAlbum`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `ShareAlbumHistory` ADD CONSTRAINT `ShareAlbumHistory_shareAlbumId_fkey` FOREIGN KEY (`shareAlbumId`) REFERENCES `ShareAlbum`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ShareAlbumInviteCode` ADD CONSTRAINT `ShareAlbumInviteCode_shareAlbumId_fkey` FOREIGN KEY (`shareAlbumId`) REFERENCES `ShareAlbum`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

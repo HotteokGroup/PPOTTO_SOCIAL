962680c7e08e09ece48c3daea03daa31b32c1177
argon1025 <argon1025@gmail.com>
2023-10-12 00:05:28 +0900

    feat: 앨범 수정 커맨드 기능 추가

---
+++ src/share-album/command/modify/modify-album.handler.ts
@@ -0,0 +1,21 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+import { ModifyShareAlbumCommand, ModifyShareAlbumCommandResult } from './modify-album.command';
+import { PrismaService } from '../../../lib/prisma/prisma.service';
+
+@CommandHandler(ModifyShareAlbumCommand)
+export class ModifyShareAlbumHandler
+  implements ICommandHandler<ModifyShareAlbumCommand, ModifyShareAlbumCommandResult>
+{
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async execute(command: ModifyShareAlbumCommand): Promise<ModifyShareAlbumCommandResult> {
+    const { id, name, bio } = command;
+    const updateResult = await this.prismaService.shareAlbum.update({
+      where: { id },
+      data: { name, bio },
+    });
+
+    return new ModifyShareAlbumCommandResult({ id: updateResult.id });
+  }
+}

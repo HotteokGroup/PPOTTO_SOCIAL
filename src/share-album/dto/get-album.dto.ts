317e2b4b13fc18690eb368d505f90b816cec2701
argon1025 <argon1025@gmail.com>
2023-10-13 00:42:11 +0900

    feat: 공유앨범 조회 API

---
+++ src/share-album/dto/get-album.dto.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ShareAlbumMemberRole } from '@prisma/client';
+import { Exclude, Expose, Type } from 'class-transformer';
+
+@Exclude()
+class GetSharedAlbumMember {
+  @Expose()
+  @ApiProperty({ description: '앨범 공유 멤버 아이디', example: 1 })
+  userId: number;
+
+  @Expose()
+  @ApiProperty({ description: '권한', enum: ShareAlbumMemberRole, example: ShareAlbumMemberRole.OWNER })
+  role: ShareAlbumMemberRole;
+
+  @Expose()
+  @ApiProperty({ description: '가입일', example: '2023-09-28 15:53:24.586' })
+  joinedAt: Date;
+}
+
+@Exclude()
+export class GetSharedAlbumResponse {
+  @Expose()
+  @ApiProperty({ description: '앨범 아이디', example: 'cln3cu9nl00043cwt43mzfro5' })
+  id: string;
+
+  @Expose()
+  @ApiProperty({ description: '앨범 명', example: '앨범' })
+  name: string;
+
+  @Expose()
+  @ApiProperty({ description: '앨범 소개', example: '내 앨범' })
+  bio: string;
+
+  @Expose()
+  @Type(() => GetSharedAlbumMember)
+  shareAlbumMember: GetSharedAlbumMember[];
+}
--- src/share-album/share-album.controller.ts
+++ src/share-album/share-album.controller.ts
@@ -1,18 +1,30 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 
 import { CreateShareAlbumCommand } from './command/create/create-album.command';
 import { ModifyShareAlbumCommand } from './command/modify/modify-album.command';
 import { CreateShareAlbumRequest, CreateShareAlbumResponse } from './dto/create-album.dto';
+import { GetSharedAlbumResponse } from './dto/get-album.dto';
 import { ModifyShareAlbumRequest, ModifyShareAlbumResponse } from './dto/modify-album.dto';
+import { GetSharedAlbumQuery } from './query/get-album/get-album.query';
 import { ERROR_CODE, GenerateSwaggerDocumentByErrorCode } from '../lib/exception/error.constant';
 
 @Controller('share-album')
 @ApiTags('공유앨범')
 export class ShareAlbumController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  @Get(':id')
+  @ApiOperation({ summary: '공유앨범 조회', description: '공유앨범을 조회합니다.' })
+  @GenerateSwaggerDocumentByErrorCode([ERROR_CODE.INTERNAL_SERVER_ERROR, ERROR_CODE.SHARE_ALBUM_NOT_FOUND])
+  async getShareAlbum(@Param('id') id: string) {
+    return plainToInstance(GetSharedAlbumResponse, await this.queryBus.execute(new GetSharedAlbumQuery({ id })));
+  }
 
   @Post()
   @ApiOperation({ summary: '공유앨범 생성', description: '공유앨범을 생성합니다.' })

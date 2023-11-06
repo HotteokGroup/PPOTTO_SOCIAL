9ee1b5b8eac7cc850648a96cf107b532d4bab6f6
argon1025 <argon1025@gmail.com>
2023-11-06 23:28:57 +0900

    feat: 피드 수정 기능

---
+++ src/share-album-feed/command/modify-feed/modify-feed.handler.ts
@@ -0,0 +1,57 @@
+import { NotFoundException } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+import { ModifyShareAlbumFeedCommand } from './modify-feed.command';
+import { ERROR_CODE } from '../../../lib/exception/error.constant';
+import { PrismaService } from '../../../lib/prisma/prisma.service';
+
+@CommandHandler(ModifyShareAlbumFeedCommand)
+export class ModifyShareAlbumFeedHandler implements ICommandHandler<ModifyShareAlbumFeedCommand, boolean> {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async execute(command: ModifyShareAlbumFeedCommand) {
+    const { feedId: id, shareAlbumId, description, contents } = command;
+
+    // 피드 존재 여부 확인
+    const feed = await this.prismaService.shareAlbumFeed.findUnique({
+      where: {
+        id,
+        shareAlbumId,
+      },
+    });
+    if (!feed) {
+      throw new NotFoundException(ERROR_CODE.SHARE_ALBUM_FEED_NOT_FOUND);
+    }
+
+    // 피드 내용 수정
+    await this.prismaService.shareAlbumFeed.update({
+      where: {
+        id,
+        shareAlbumId,
+      },
+      data: {
+        description,
+        FeedContent: {
+          updateMany: {
+            where: {
+              feedId: id,
+            },
+            data: {
+              deletedAt: new Date(),
+            },
+          },
+          createMany: {
+            data: contents.map((content) => ({
+              contentId: content.contentId,
+              type: content.type,
+              contentLargeUrl: content.contentLargeUrl,
+              contentMediumUrl: content.contentMediumUrl,
+              contentSmallUrl: content.contentSmallUrl,
+            })),
+          },
+        },
+      },
+    });
+    return true;
+  }
+}

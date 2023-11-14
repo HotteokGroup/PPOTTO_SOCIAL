379d1aefe0ffa88ca63d7858770ca60e3f440676
argon1025 <argon1025@gmail.com>
2023-11-14 08:46:49 +0000

    feat: 에러코드 추가

--- src/lib/exception/error.constant.ts
+++ src/lib/exception/error.constant.ts
@@ -76,6 +76,11 @@ export const ERROR_CODE = dynamicRecord({
     message: '컬렉션을 찾을 수 없습니다.',
     status: HttpStatus.NOT_FOUND,
   },
+  COLLECTION_FEED_NOT_FOUND: {
+    errorCode: 'COLLECTION_FEED_NOT_FOUND',
+    message: '컬렉션에 해당 피드가 없습니다.',
+    status: HttpStatus.NOT_FOUND,
+  },
 });
 
 // 스웨거 Exception Description을 위한 데코레이터
